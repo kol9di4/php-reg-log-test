0a68be665e2a47c8d1e024b0ab2ad385e3df2bfe
kol9di4 <76259753+kol9di4@users.noreply.github.com>
2024-04-09 10:52:27 +0000

    one

---
+++ Exceptions.php
@@ -0,0 +1,6 @@
+<?php
+
+class LoginException extends Exception{}
+class PasswordException extends Exception{}
+class EmailException extends Exception{}
+class NameException extends Exception{}
---
+++ User.php
@@ -0,0 +1,8 @@
+<?php
+class User{
+    protected string $login;
+    protected string $password;
+    protected string $email;
+    protected string $name;
+    
+}
