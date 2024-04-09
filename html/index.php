d92c5307a1e66ba2b10491d87af2802fd23c5a9f
kol9di4 <76259753+kol9di4@users.noreply.github.com>
2024-04-09 11:17:03 +0000

    one

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once("FileStorage.php");
+
+$dbConnecton = FileStorage::getInstance('DataBase/db.json');
+
+// $dbConnecton->create([
+//     'login' => 'admin',
+//     'pasword' => 'admin',
+//     'email' => 'admin@admin',
+//     'name' => 'Vasya'
+// ]);
+
+
+echo '<pre>';
+var_dump($dbConnecton->get(1));
+echo '</pre>';
